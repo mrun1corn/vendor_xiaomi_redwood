cd691f553a81222da040c841396cc71933081e28
Thereache <thereache2003@gmail.com>
2025-05-12 18:48:17 +0000

    lfs

--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-cat vendor/xiaomi/redwood-miuicamera/common/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk.part* > vendor/xiaomi/redwood-miuicamera/common/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk
-cat vendor/xiaomi/redwood-miuicamera/prebuilts/MiuiVideoPlayer/MiuiVideoPlayer.apk.part* > vendor/xiaomi/redwood-miuicamera/prebuilts/MiuiVideoPlayer/MiuiVideoPlayer.apk
